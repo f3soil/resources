670e7b348c66211edcc22cef423a61df8d35f0af
Nathaniel Nutter <iam@nnutter.com>
2023-03-16 20:55:35 -0500

    Incorporate home AO into user-not-migrated

--- queries/user-not-migrated.sql
+++ queries/user-not-migrated.sql
@@ -1,4 +1,15 @@
 WITH
+    attendance_counts AS (
+        SELECT PAX, AO, COUNT(*) AS Count
+        FROM f3stlcity.attendance_view
+        GROUP BY PAX, AO
+    ),
+    home_ao AS (
+        SELECT a.PAX, a.AO AS HomeAO
+        FROM attendance_counts AS a
+        LEFT JOIN attendance_counts AS b ON a.PAX = b.PAX AND a.Count < b.Count
+        WHERE b.Count IS NULL
+    ),
     soil_bd_attendance AS (
         SELECT user_id,
                COUNT(*) AS beatdowns
@@ -17,13 +28,14 @@ WITH
         GROUP BY user_id
     )
 SELECT
-    CONCAT('@', u.user_name) AS user_name
+    h.HomeAO, u.user_name, u.real_name, u.email
 FROM soil_bd_attendance AS bd
 JOIN f3stlcity.users AS u ON u.user_id = bd.user_id
+JOIN home_ao AS h ON u.user_name = h.PAX
 LEFT JOIN f3soil.users AS soil1 ON (u.user_name = soil1.user_name)
 LEFT JOIN f3soil.users AS soil2 ON (u.email = soil2.email)
 WHERE
-        beatdowns >= 5
+        beatdowns > 1
   AND COALESCE(soil1.user_name, soil2.email) IS NULL
 ORDER BY
-    user_name
+    HomeAO, user_name
