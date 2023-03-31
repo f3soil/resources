b984ef7dac93efb73dff32b0b82685a05e54be61
Nathaniel Nutter <n@nutter.xyz>
2023-03-31 08:01:39 -0500

    Update backblast-aos with the latest custom channels.

--- queries/backblast-aos.sql
+++ queries/backblast-aos.sql
@@ -2,7 +2,13 @@ UPDATE aos
 SET backblast = 1
 WHERE (
     ao LIKE 'ao_%'
-    OR channel_id = 'C04KMV5DP32' -- black_ops
+    OR channel_id IN (
+      'C04KMV5DP32', -- black_ops
+      'C04KMV5DP32', -- dr
+      'C04HUNJC05B', -- q-source
+      'C04HR3BH9K8', -- rucking
+      'C0518AEDD7B'  -- running
+    )
   )
   AND backblast != 1
   AND archived = 0
