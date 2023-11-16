5ef16ef9dd3503e39778e18e60625e3a87733f79
Nathaniel Nutter <iam@nnutter.com>
2023-11-16 11:30:15 -0600

    Update FNGs query

--- queries/fngs.sql
+++ queries/fngs.sql
@@ -9,15 +9,11 @@ SELECT
     bd.fngs AS FNGs,
     bb.backblast,
     GROUP_CONCAT(pax.PAX) AS PAX
-FROM beatdown_info AS bd
-JOIN attendance_view AS pax ON (bd.Date = pax.Date AND bd.AO = pax.AO AND bd.Q = pax.Q)
-JOIN backblast bb ON (bd.AO = bb.AO AND bd.Date = bb.Date)
+FROM f3stlcity.beatdown_info AS bd
+JOIN f3stlcity.attendance_view AS pax ON (bd.Date = pax.Date AND bd.AO = pax.AO AND bd.Q = pax.Q)
+JOIN f3stlcity.backblast bb ON (bd.AO = bb.AO AND bd.Date = bb.Date)
 WHERE
-    bd.fng_count > 0
-    AND bd.fngs != "0"
-    AND bd.fngs NOT LIKE "None%"
-    AND bd.fngs NOT LIKE "none%"
-    AND bd.fngs NOT LIKE "Nope%"
+    bd.AO LIKE "%zoo%"
 GROUP BY
     bd.Date,
     bd.AO,
