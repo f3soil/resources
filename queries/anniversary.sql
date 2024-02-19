c5152f4d283a5713667cda6bcdb84fd451814e7d
Nathaniel Nutter <iam@nnutter.com>
2024-02-19 07:44:54 -0600

    Add query to lookup a PAX's anniversary

---
+++ queries/anniversary.sql
@@ -0,0 +1,8 @@
+set @user_name = 'Roughneck';
+set @user_id = (select user_id from f3soil.users where user_name = @user_name);
+select *
+from f3soil.backblast
+where backblast like concat('%', @user_id, '%')
+   or backblast like concat('%', @user_name, '%')
+order by Date
+    limit 3;
