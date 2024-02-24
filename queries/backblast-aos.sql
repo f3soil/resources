103b854f900766f874301185b594df5dba22fcb2
Nathaniel Nutter <iam@nnutter.com>
2024-02-24 11:57:58 -0600

    Update backblast-aos query

--- queries/backblast-aos.sql
+++ queries/backblast-aos.sql
@@ -1,20 +1,8 @@
 UPDATE aos
-SET backblast = 1
-WHERE (
-    ao LIKE 'ao_%'
-    OR ao LIKE 'csaup_%'
-    OR ao LIKE 'csaups_%'
-    OR ao LIKE 'black_ops%'
-    OR channel_id IN (
-      'C04KMV5DP32', -- dr
-      'C04HUNJC05B', -- q-source
-      'C04HR3BH9K8', -- rucking
-      'C0518AEDD7B'  -- running
-    )
-  )
-  AND backblast != 1
-  AND archived = 0;
-SELECT ao
+SET backblast = IF(ao LIKE 'mumblechat%' OR archived = 1, 0, 1)
+WHERE
+    backblast = IF(ao LIKE 'mumblechat%' OR archived = 1, 1, 0);
+
+SELECT ao, backblast, archived
 FROM aos
-WHERE backblast = 1 AND archived = 0
-ORDER BY ao;
+ORDER BY backblast DESC, archived, ao;
