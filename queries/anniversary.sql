95a0d6c51d81e0cc47200f03e29ee2e2f0b1f8e5
Nathaniel Nutter <iam@nnutter.com>
2024-03-23 20:18:34 -0500

    Show user_id/user_name

--- queries/anniversary.sql
+++ queries/anniversary.sql
@@ -1,6 +1,8 @@
 set @user_name = 'Roughneck';
 set @user_id = (select user_id from f3soil.users where user_name = @user_name);
-select *
+select @user_id, @user_name;
+select
+    *
 from f3soil.backblast
 where backblast like concat('%', @user_id, '%')
    or backblast like concat('%', @user_name, '%')
