a7d7c1d23e7a1a02c96c212916c9e40d213f9061
Nathaniel Nutter <iam@nnutter.com>
2024-04-29 17:30:50 -0500

    Update backblast definition

--- queries/backblast-aos.sql
+++ queries/backblast-aos.sql
@@ -1,8 +1,16 @@
 UPDATE aos
-SET backblast = IF(ao LIKE 'mumblechat%' OR archived = 1, 0, 1)
+SET backblast = IF(ao LIKE '%mumblechat%' OR ao IN ('announcements', 'sector_discussion') OR archived, 0, 1)
 WHERE
-    backblast = IF(ao LIKE 'mumblechat%' OR archived = 1, 1, 0);
+    backblast != IF(ao LIKE '%mumblechat%' OR ao IN ('announcements', 'sector_discussion') OR archived, 0, 1);
 
-SELECT ao, backblast, archived
+SELECT ao, channel_id, archived, backblast
 FROM aos
-ORDER BY backblast DESC, archived, ao;
+ORDER BY ao;
+
+UPDATE aos
+SET archived = 1
+WHERE
+    channel_id IN (
+        'C05K2TKMY2H',
+        'C06NFKQL7MW'
+    );
