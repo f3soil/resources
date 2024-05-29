12c75cf37425234a97329098e1d8521650e723cd
Nathaniel Nutter <iam@nnutter.com>
2024-05-29 17:18:43 -0500

    Update backblast-aos.sql

--- queries/backblast-aos.sql
+++ queries/backblast-aos.sql
@@ -1,16 +1,18 @@
 UPDATE aos
-SET backblast = IF(ao LIKE '%mumblechat%' OR ao IN ('announcements', 'sector_discussion') OR archived, 0, 1)
-WHERE
-    backblast != IF(ao LIKE '%mumblechat%' OR ao IN ('announcements', 'sector_discussion') OR archived, 0, 1);
-
-SELECT ao, channel_id, archived, backblast
-FROM aos
-ORDER BY ao;
+SET   backblast  = IF(ao LIKE '%mumblechat%' OR ao LIKE '%slt' OR ao IN ('paxminer_logs', 'sector_discussion', 'slt-messages') OR archived, 0, 1)
+WHERE backblast != IF(ao LIKE '%mumblechat%' OR ao LIKE '%slt' OR ao IN ('paxminer_logs', 'sector_discussion', 'slt-messages') OR archived, 0, 1)
+;
 
 UPDATE aos
 SET archived = 1
-WHERE
-    channel_id IN (
-        'C05K2TKMY2H',
-        'C06NFKQL7MW'
-    );
+WHERE channel_id IN (
+    'C04HH51JF4P',
+    'C05K2TKMY2H',
+    'C06NFKQL7MW',
+    'C06UDUBH160'
+);
+
+SELECT ao, channel_id, archived, backblast
+FROM aos
+WHERE NOT archived OR (archived AND backblast)
+ORDER BY ao;
